a7ede52e1fd3638b26fd54e5d1635ace6a15e8bc
hallernsk <avladim@mail.ru>
2024-07-01 16:01:42 +0700

    fixed migration(article_tag_table)

--- migrations/m240626_133238_create_article_tag_table.php
+++ migrations/m240626_133238_create_article_tag_table.php
@@ -14,7 +14,45 @@ class m240626_133238_create_article_tag_table extends Migration
     {
         $this->createTable('{{%article_tag}}', [
             'id' => $this->primaryKey(),
+            'article_id' => $this->integer(),
+            'tag_id' => $this->integer()
         ]);
+
+        // creates index for column `article_id`
+        $this->createIndex(
+            'tag_article_article_id',
+            'article_tag',
+            'article_id'
+        );
+
+
+        // add foreign key for table `article`
+        $this->addForeignKey(
+            'tag_article_article_id',
+            'article_tag',
+            'article_id',
+            'article',
+            'id',
+            'CASCADE'
+        );
+
+        // creates index for column `tag_id`
+        $this->createIndex(
+            'idx_tag_id',
+            'article_tag',
+            'tag_id'
+        );
+
+
+        // add foreign key for table `tag`
+        $this->addForeignKey(
+            'fk-tag_id',
+            'article_tag',
+            'tag_id',
+            'tag',
+            'id',
+            'CASCADE'
+        );
     }
 
     /**
