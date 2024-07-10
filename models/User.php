ee94ca21f10f5d96b24c16f9a8ec72ce7d2aed6c
hallernsk <avladim@mail.ru>
2024-07-10 13:40:24 +0700

    fixed auth

--- models/User.php
+++ models/User.php
@@ -49,6 +49,32 @@ class User extends \yii\db\ActiveRecord implements IdentityInterface
         // return $this->authKey === $authKey;
     }
 
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return static|null
+     */
+
+    public static function findByUsername($username)
+    {
+        return User::find()->where(['name' => $username])->one();
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
+    }
+
+
+
+
     // Other methods of your User class
 
     /**
